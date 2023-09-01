ffc8ae9ba4b57d9c251e57be9314eb5c97e43c28
Deepak Yadav <deepak.yadav@polymerupdate.com>
2023-09-01 18:30:25 +0530

    change backend path

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL = "https://words-will-help-backend.onrender.com/";
+let BASE_URL = "https://backend-tp83.onrender.com/";
 // if (!process.env.NODE_ENV || process.env.NODE_ENV === "production") {
 //   BASE_URL = "https://words-will-help-backend.onrender.com/";
 // }
